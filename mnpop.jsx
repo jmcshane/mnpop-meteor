b1501e826a6e8ca47706031c9ebd4d14d565387e
James McShane <jmcshan1@gmail.com>
2016-02-24 01:11:42 -0500

    Initial mockup of first page.  Removed mongo for deployment to galaxy.

---
+++ mnpop.jsx
@@ -0,0 +1,25 @@
+if (Meteor.isClient) {
+  Meteor.startup(function() {
+    ReactDOM.render(<App />, document.getElementById("render-target"));
+    WebFontConfig = {
+    	google: {families : ['Roboto Slab:700,400:latin','Oswald:400', 'Mouse Memoirs']}
+    };
+    (function() {
+	    var wf = document.createElement('script');
+	    wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+	      '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+	    wf.type = 'text/javascript';
+	    wf.async = 'true';
+	    var s = document.getElementsByTagName('script')[0];
+	    s.parentNode.insertBefore(wf, s);
+	    console.log("async fonts loaded", WebFontConfig);    	
+    });
+  });
+}
+
+if (Meteor.isServer) {
+
+}
+
+//NavElts = new Mongo.Collection("navelts")
+//AuthenticatedUsers = new Mongo.Collection("authusers")
